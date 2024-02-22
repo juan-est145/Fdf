7c50b1177692f6e2e6471948243fcdf56644a19d
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-02-22 15:20:26 +0100

    Updated libray to allowed version

---
+++ functions-for-colors.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions-for-colors.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 14:29:52 by juestrel          #+#    #+#             */
+/*   Updated: 2024/02/22 14:35:32 by juestrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Ultimate_Libft/libft.h"
+#include "MLX42/include/MLX42/MLX42.h"
+#include "fdf.h"
+
+//This function allows to return the color that you want using rgba encoding.
+//The individual bits are moved to their corresponding channel (red, green, blue or alpha)
+//and returns a integer codified with said values.
+//The '|' operator here allows us to combine the values in a single integer
+int get_rgba(int r, int g, int b, int a) 
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 19:44:36 by juestrel          #+#    #+#             */
-/*   Updated: 2024/02/21 20:19:11 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/02/22 14:33:23 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include "MLX42/include/MLX42/MLX42.h"
 #include "Ultimate_Libft/libft.h"
-#include  <stdio.h> // Borrar luego
+#include "fdf.h"
 
 /*void	ft_leaks(void)
 {
@@ -32,7 +32,7 @@ int	main(void)
 	mlx_start = mlx_init(512, 512, "FDF", true); //Crea la instancia que lleva el programa
     img = mlx_new_image(mlx_start, 128, 128); //Crea una nueva imagen
     mlx_image_to_window(mlx_start, img, 0, 0); //Cárgala a la ventana la imagen
-    mlx_put_pixel(img, 10, 5, 0xEDCE9B); //Pinta pixels
+    mlx_put_pixel(img, 10, 5, get_rgba(181, 0, 185, 255)); //Pinta pixels
 	mlx_loop(mlx_start); //Arranca el bucle de la pantalla
 	mlx_terminate(mlx_start); // Cierra todo cuando se hace click en cerrar
 	return (0);
