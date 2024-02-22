c9c56926b733eb0bd3113382589e4ce2f1f2484b
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-02-22 16:14:19 +0100

    Function for being able to paint the entire image in one color has been found

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 19:44:36 by juestrel          #+#    #+#             */
-/*   Updated: 2024/02/22 14:33:23 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/02/22 16:13:33 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,24 @@ int	main(void)
 
 	mlx_set_setting(MLX_STRETCH_IMAGE, true); //Esto hace que la pantalla se vuelva completa
 	mlx_start = mlx_init(512, 512, "FDF", true); //Crea la instancia que lleva el programa
-    img = mlx_new_image(mlx_start, 128, 128); //Crea una nueva imagen
+    img = mlx_new_image(mlx_start, 250, 250); //Crea una nueva imagen
     mlx_image_to_window(mlx_start, img, 0, 0); //Cárgala a la ventana la imagen
-    mlx_put_pixel(img, 10, 5, get_rgba(181, 0, 185, 255)); //Pinta pixels
+    for (size_t x = 0; x < img->width; x++)
+	{
+		for (size_t y = 0; y < img->height; y++)
+		{
+			mlx_put_pixel(img, x, y, get_rgba(255, 0, 0, 255));
+		}
+	}	
 	mlx_loop(mlx_start); //Arranca el bucle de la pantalla
 	mlx_terminate(mlx_start); // Cierra todo cuando se hace click en cerrar
 	return (0);
 }
+
+/*mlx_set_setting(MLX_STRETCH_IMAGE, true); //Esto hace que la pantalla se vuelva completa
+	mlx_start = mlx_init(512, 512, "FDF", true); //Crea la instancia que lleva el programa
+    img = mlx_new_image(mlx_start, 128, 128); //Crea una nueva imagen
+    mlx_image_to_window(mlx_start, img, 0, 0); //Cárgala a la ventana la imagen
+    mlx_put_pixel(img, 10, 5, get_rgba(181, 0, 185, 255)); //Pinta pixels
+	mlx_loop(mlx_start); //Arranca el bucle de la pantalla
+	mlx_terminate(mlx_start); // Cierra todo cuando se hace click en cerrar*/
