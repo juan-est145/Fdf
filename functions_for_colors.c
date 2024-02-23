e1f478e8087f8cdf6c269442747c07154d82e52e
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-02-23 17:45:56 +0100

    Checked that the reading struct counts correctly the number of numbers present in each line even when paired with colors

--- functions_for_colors.c
+++ functions_for_colors.c
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   functions-for-colors.c                             :+:      :+:    :+:   */
+/*   functions_for_colors.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 14:29:52 by juestrel          #+#    #+#             */
-/*   Updated: 2024/02/22 16:44:49 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/02/23 17:37:31 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Ultimate_Libft/libft.h"
 #include "MLX42/include/MLX42/MLX42.h"
+#include "Ultimate_Libft/libft.h"
 #include "fdf.h"
 
-//This function allows to return the color that you want using rgba encoding.
-//The individual bits are moved to their corresponding channel (red, green, blue or alpha)
-//and returns a integer codified with said values.
-//The '|' operator here allows us to combine the values in a single integer
-//For each of the int parameters, just use values between 0 and 255
-int get_rgba(int r, int g, int b, int a) 
+// This function allows to return the color that you want using rgba encoding.
+// The individual bits are moved to their
+// corresponding channel (red, green, blue or alpha)
+// and returns a integer codified with said values.
+// The '|' operator here allows us to combine the values in a single integer
+// For each of the int parameters, just use values between 0 and 255
+int	get_rgba(int r, int g, int b, int a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
