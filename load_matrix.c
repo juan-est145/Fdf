3fdbc86aecc036ae278fecbac9ef4f57eb782213
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-02-23 19:29:45 +0100

    First map seems to be able to print store the values correctly

--- load_matrix.c
+++ load_matrix.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:19:24 by juestrel          #+#    #+#             */
-/*   Updated: 2024/02/23 18:34:40 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/02/23 19:29:23 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@
 
 static void	process_map_file(t_map_line_read **head, int fd,
 				unsigned int *y_counter);
+static void	create_matrix_of_coord(t_map_line_read **head, t_coord **map);
 
 void	load_matrix(char *path)
 {
 	int				fd;
 	t_map_line_read	*head;
 	unsigned int	y_counter;
+	t_coord			**map;
 
 	head = NULL;
 	y_counter = 0;
@@ -31,6 +33,10 @@ void	load_matrix(char *path)
 	if (fd < 0)
 		print_error_msg(FAILURE_TO_OPEN_FILE);
 	process_map_file(&head, fd, &y_counter);
+	map = (t_coord **)malloc(sizeof(t_coord *) * y_counter);
+	if (map == NULL)
+		malloc_error(&head);
+	create_matrix_of_coord(&head, map);
 	close(fd);
 }
 
@@ -63,8 +69,34 @@ static void	process_map_file(t_map_line_read **head, int fd,
 		(*y_counter)++;
 	}
 }
-// Tester function for checking that the read is successful
 
+static void	create_matrix_of_coord(t_map_line_read **head, t_coord **map)
+{
+	t_map_line_read	*temp;
+	unsigned int	y;
+	unsigned int	x;
+	unsigned int	number_str_index;
+
+	temp = *head;
+	y = 0;
+	while (temp->next != NULL)
+	{
+		x = 0;
+		number_str_index = 0;
+		map[y] = (t_coord *)malloc(sizeof(t_coord) * temp->x_length);
+		while (temp->coord[number_str_index] != NULL && temp->coord[number_str_index][0] != '\n') //Change condition (index is good)
+		{
+			map[y][x].value_of_z = ft_atoi(temp->coord[number_str_index]); //Need to check for optional colors
+			x++;
+			number_str_index++;
+		}
+		temp = temp->next;
+		y++;
+	}
+	free_map_line(head);
+}
+
+// Tester function for checking that the read is successful
 /*static void	tester(t_map_line_read **head); // Erase later
 
 static void	tester(t_map_line_read **head)
