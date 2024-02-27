73161e7e00f8b838e24a03a3dfb7f631eff632b5
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-02-27 13:43:43 +0100

    I can create a perfect colored image of the map, but not the lines that connect it

--- src/input_to_screen.c
+++ src/input_to_screen.c
@@ -6,13 +6,15 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:41:27 by juestrel          #+#    #+#             */
-/*   Updated: 2024/02/26 18:50:52 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/02/27 13:31:55 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../MLX42/include/MLX42/MLX42.h"
 #include "../Ultimate_Libft/libft.h"
 #include "fdf.h"
+
+static void	put_pixels(t_map_data **map_data, mlx_image_t **img);
 
 void	input_to_screen(t_map_data **map_data)
 {
@@ -31,7 +33,30 @@ void	input_to_screen(t_map_data **map_data)
 		destroy_map_data((*map_data)->map, map_data);
 		print_error_msg(MLX_INIT_FAILURE);
 	}
+	put_pixels(map_data, &img);
 	mlx_loop(mlx_start);
 	mlx_terminate(mlx_start);
 	destroy_map_data((*map_data)->map, map_data);
+}
+
+static void	put_pixels(t_map_data **map_data, mlx_image_t **img)
+{
+	unsigned int	y;
+	unsigned int	x;
+
+	y = 0;
+	while (y < (*map_data)->height)
+	{
+		if ((*map_data)->map[y][x].color_present == false)
+			mlx_put_pixel(*img, x, y, 255);
+		else if ((*map_data)->map[y][x].color_present == true)
+			mlx_put_pixel(*img, x, y, (*map_data)->map[y][x].color);
+		if ((*map_data)->map[y][x].end_of_row == true)
+		{
+			x = 0;
+			y++;
+		}
+		else
+			x++;
+	}
 }
