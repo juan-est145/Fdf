c66c8adfd154e2c96f9167e6391a0251d45f564c
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-03-14 16:31:49 +0100

    We have straight lines

--- src/input_to_screen.c
+++ src/input_to_screen.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:41:27 by juestrel          #+#    #+#             */
-/*   Updated: 2024/03/14 14:10:03 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/03/14 16:31:22 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ static void	put_pixels(t_map_data **map_data, mlx_image_t **img);
 static void	bresenham(t_bresenham_coord coord, t_map_data **map_data,
 				mlx_image_t **img, int color);
 static int	select_color(t_map_data **map_data, unsigned int x, unsigned int y);
-static void	isometric_projection(t_bresenham_coord *coord,
-				t_map_data **map_data);
+/*static void	isometric_projection(t_bresenham_coord *coord,
+				t_map_data **map_data);*/
+static void slope_less_than_one(t_bresenham_coord coord, mlx_image_t **img, int color);
+static void slope_bigger_than_one(t_bresenham_coord coord, mlx_image_t **img, int color);
 
 void	input_to_screen(t_map_data **map_data)
 {
@@ -80,14 +82,18 @@ static int	select_color(t_map_data **map_data, unsigned int x, unsigned int y)
 static void	bresenham(t_bresenham_coord coord, t_map_data **map_data,
 		mlx_image_t **img, int color)
 {
-	float	x_increase;
-	float	y_increase;
+	//float	x_increase;
+	//float	y_increase;
 
 	coord = zoom_multiplier(coord, map_data);
-	isometric_projection(&coord, map_data);
+	//isometric_projection(&coord, map_data);
 	coord.delta_x = (coord.x_next - coord.x);
 	coord.delta_y = (coord.y_next - coord.y);
-	x_increase = calculate_increase(fabsf(coord.delta_x), fabsf(coord.delta_x), //Added fabsf to both calculate increase
+	if (fabsf(coord.delta_x) > fabsf(coord.delta_y))
+		slope_less_than_one(coord, img, color);
+	else
+		slope_bigger_than_one(coord, img, color);
+	/*x_increase = calculate_increase(fabsf(coord.delta_x), fabsf(coord.delta_x), //Added fabsf to both calculate increase
 			fabsf(coord.delta_y));
 	y_increase = calculate_increase(fabsf(coord.delta_y), fabsf(coord.delta_x),
 			fabsf(coord.delta_y));
@@ -96,10 +102,10 @@ static void	bresenham(t_bresenham_coord coord, t_map_data **map_data,
 		mlx_put_pixel(*img, coord.x, coord.y, color);
 		coord.x += x_increase;
 		coord.y += y_increase;
-	}
+	}*/
 }
 
-static void	isometric_projection(t_bresenham_coord *coord,
+/*static void	isometric_projection(t_bresenham_coord *coord,
 		t_map_data **map_data)
 {
 	unsigned int	tmp;
@@ -117,4 +123,62 @@ static void	isometric_projection(t_bresenham_coord *coord,
 
 	//One of the problems I seem to have is that I do not add a zoom to the z value before doing the isometric
 	//projection
+}*/
+
+static void slope_less_than_one(t_bresenham_coord coord, mlx_image_t **img, int color)
+{
+	int p;
+	int i;
+
+	i = -1;
+	p = 2 * fabsf(coord.delta_y) - fabsf(coord.delta_x);
+	mlx_put_pixel(*img, coord.x, coord.y, color);
+	while (++i < fabsf(coord.delta_x))
+	{
+		if (coord.delta_x > 0)
+			coord.x += 1;
+		else
+			coord.x -= 1;
+		if (p < 0)
+			p = p + 2 * fabsf(coord.delta_y);
+		else
+		{
+			if (coord.delta_y > 0)
+				coord.y += 1;
+			else
+				coord.y -= 1;
+			p = p + 2 * fabsf(coord.delta_y) - 2 * fabsf(coord.delta_x);
+		}
+		mlx_put_pixel(*img, coord.x, coord.y, color);
+		i++;
+	}
+}
+
+static void slope_bigger_than_one(t_bresenham_coord coord, mlx_image_t **img, int color)
+{
+	int p;
+	int i;
+
+	i = -1;
+	p = 2 * fabsf(coord.delta_x) - fabsf(coord.delta_y);
+	mlx_put_pixel(*img, coord.x, coord.y, color);
+	while (++i < fabsf(coord.delta_y))
+	{
+		if (coord.delta_y > 0)
+  			coord.y += 1;
+		else
+			coord.y -= 1;
+		if (p < 0)
+		p = p + 2 * fabsf(coord.delta_x);
+		else
+		{
+			if (coord.delta_x > 0)
+				coord.x += 1;
+			else
+				coord.x -= 1;
+			p = p + 2 * fabsf(coord.delta_x) - 2 * fabsf(coord.delta_y);
+		}
+		mlx_put_pixel(*img, coord.x, coord.y, color);
+		i++;
+	}
 }
